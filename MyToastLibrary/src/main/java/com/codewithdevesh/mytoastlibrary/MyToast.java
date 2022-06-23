2ae0ab78fcf872072548ae764d87f818998f4eb3
Devesh <deveshparmar2003@gmail.com>
2022-06-23 11:09:49 +0530

    Initial Commit

---
+++ MyToastLibrary/src/main/java/com/codewithdevesh/mytoastlibrary/MyToast.java
@@ -0,0 +1,61 @@
+package com.codewithdevesh.mytoastlibrary;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class MyToast {
+    public static void showSimpleToast(Context context,String message){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.simple_toast_layout,null);
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setText(message);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.BOTTOM,0,0);
+        toast.setView(view);
+        toast.show();
+    }
+    public static void showSuccessToast(Context context,String message){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.success_toast_layout,null);
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setText(message);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.BOTTOM,0,0);
+        toast.setView(view);
+        toast.show();
+    }
+    public static void showWarningToast(Context context,String message){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.warning_toast_layout,null);
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setText(message);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.BOTTOM,0,0);
+        toast.setView(view);
+        toast.show();
+    }
+    public static void showErrorToast(Context context,String message){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.error_toast_layout,null);
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setText(message);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.BOTTOM,0,0);
+        toast.setView(view);
+        toast.show();
+    }
+    public static void showInfoToast(Context context,String message){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.info_toast_layout,null);
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setText(message);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.BOTTOM,0,0);
+        toast.setView(view);
+        toast.show();
+    }
+}
